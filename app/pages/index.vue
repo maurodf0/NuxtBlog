566f398597275d42775d18f56f40ce3d6ec87201
Mauro <maurodefalco96@gmail.com>
2024-10-15 00:18:00 +0200

    useSeoMeta composable Nuxt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,3 +3,31 @@
         Hello from Nuxt
     </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Mauro De Falco',
+  description: 'Some New Nuxt Content',
+  ogTitle: '[og:title]',
+  ogDescription: '[og:description]',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'it'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
