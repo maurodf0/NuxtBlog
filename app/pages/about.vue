5e4481333a7243635ffc6af0fc9b69f029c459bd
Mauro <maurodefalco96@gmail.com>
2024-10-14 23:55:32 +0200

    change layout based on events

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,11 +1,23 @@
 <template>
     <h1>About me!</h1>
+    <button @click="enableCustomLayout">Update Layout</button>
+    <button @click="defaultLayout">Go Back Layout</button>
 </template>
 
 <script setup lang="ts">
 
-    definePageMeta({
-        layout: 'another'
-    })
+    //change layout dinamically
+    const enableCustomLayout = () => {
+        setPageLayout('another');
+    }
+
+    const defaultLayout = () => {
+        setPageLayout('default');
+    }
+
+    //change layout ofr single page
+    // definePageMeta({
+    //     layout: 'another'
+    // })
 
 </script>
