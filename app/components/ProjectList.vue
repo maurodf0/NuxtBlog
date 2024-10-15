48ebeb3631a5213d3c0412c817b22ad94b4a7fd6
Mauro <maurodefalco96@gmail.com>
2024-10-15 23:23:32 +0200

    display Github project with API and v-for

--- app/components/ProjectList.vue
+++ app/components/ProjectList.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>ProjectList</div>
+    <div>
+        <p class="mb-10">Take a looh at my GitHub Projects</p>
 
-    <section v-if="pending">Loading...</section>
-    <section v-else-if="error">Something went wrong, try again...</section>
-    <section v-else >Here display the data</section>
+        <section v-if="pending">Loading...</section>
+        <section v-else-if="error">Something went wrong, try again...</section>
+        <section v-else >
+            <ul class="grid grid-cols-1 gap-4">
+                <li 
+                    v-for="repo in repos" 
+                    class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+                    :key="repo.id">
+                    <a :href="repo.html_url" target="_blank">
+                        <div class="flex items-center justify-between text-sm">
+                            <div class="font-semibold">{{ repo.name }}</div>
+                            <h6> {{ repo.stargazers_count }} ★</h6>
+                        </div>  
+                        <p class="text-sm">{{ repo.description }}</p>
+                    </a>
+                </li>
+            </ul>
+        </section>
+    </div>
 </template>
 
 <script setup>
 const { error, pending, data } = await useFetch('https://api.github.com/users/maurodf0/repos')
-console.log(await useFetch('https://api.github.com/users/maurodf0/repos'));
+
+const repos = computed( () => data.value.filter( repo => repo.description ).sort((a,b ) => b.stargazers_count - a.stargazers_count) )
 
 </script>   
