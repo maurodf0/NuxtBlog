a9a7f315bd1fd87c32da4b0361aa0630a98deaab
Mauro <maurodefalco96@gmail.com>
2024-10-15 23:24:08 +0200

    comment added

--- app/components/ProjectList.vue
+++ app/components/ProjectList.vue
@@ -26,6 +26,8 @@
 <script setup>
 const { error, pending, data } = await useFetch('https://api.github.com/users/maurodf0/repos')
 
+
+//display only repo with description and sorted by stars number
 const repos = computed( () => data.value.filter( repo => repo.description ).sort((a,b ) => b.stargazers_count - a.stargazers_count) )
 
 </script>   
