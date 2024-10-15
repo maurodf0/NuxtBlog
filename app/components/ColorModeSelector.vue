6c2710d18a2d9e0e83a5d4fcd650b23a89d3975f
Mauro <maurodefalco96@gmail.com>
2024-10-16 00:18:52 +0200

    toggle between colormode

---
+++ app/components/ColorModeSelector.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+       <button @click="toggleMode"> {{ nextMode }}</button>
+    </div>
+</template>
+
+<script setup>
+
+const colorMode = useColorMode();
+
+const modes = [
+    'system',
+    'light',
+    'dark'
+]
+
+const nextMode = computed( () => {
+    const currentModeIndex = modes.indexOf(colorMode.preference);
+    let nextModeIndex = null
+    if(currentModeIndex + 1 === modes.length){
+        nextModeIndex = 0
+    } else {
+     nextModeIndex = currentModeIndex + 1
+    }
+
+    return modes[nextModeIndex];
+});
+
+const toggleMode = () => {
+    return colorMode.preference = nextMode.value;
+}
+
+</script>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="container mx-auto max-w-2xl">   
         <header class="flex justify-between mt-5">
-            <div>
-                <NuxtLink
-                 to="/" 
-                 class="text-xl font-semibold p-2 hover:bg-gray-200">
-                 Mauro De Falco
-                </NuxtLink>
-            </div>
-            <Menu />
+          <div class="flex items-center space-x-12">
+              <div>
+                  <NuxtLink
+                  to="/" 
+                  class="text-xl font-semibold p-2 hover:bg-gray-200">
+                  Mauro De Falco
+                  </NuxtLink>
+              </div>
+              <Menu />
+          </div>
+          <ColorModeSelector />
         </header>
         <main class="p-2 mt-10">
          <slot />
