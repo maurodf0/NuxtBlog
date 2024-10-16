bd1cedbea42f794d6118ddaf146f37c882ec0299
Mauro <maurodefalco96@gmail.com>
2024-10-16 20:44:23 +0200

    querying use queryContent

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>All the blog list</h1>
+    <div v-if="result">
+        <section>
+        <ul>
+            <li class="rounded-lg border-gray-700 dark:border-gray-100 mb-4 border py-4 px-4"
+                v-for="(post, index) in result" 
+                :key="post.title"> 
+             <NuxtLink :to="post._path" >   {{ index }} - {{ post.title }} </NuxtLink> 
+            </li>
+        </ul>
+    </section>
+    </div>
+    <div v-else>Nothing Found</div>
 </template>
+
+<script setup>
+ const result = await queryContent('/blog').only(['_path', 'title']).find()
+
+</script>
