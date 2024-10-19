226d7bb73b5c23a18a62040140b3cca19f3cecf3
Mauro <maurodefalco96@gmail.com>
2024-10-19 18:42:12 +0200

    wraper contentdoc in article tag

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,5 +1,5 @@
 <template>
-    <ContentDoc 
-       class="mb-10 prose dark:prose-invert max-w-none" 
-       path="/projects" />
+    <article class="mb-10 prose dark:prose-invert max-w-none" >
+    <ContentDoc path="/projects" />
+    </article>
 </template>
