6a040ca5f619a06635f3aa26b43a29c2a881c14b
Mauro <maurodefalco96@gmail.com>
2024-10-20 18:31:45 +0200

    working

--- app/components/content/TocLinks.vue
+++ app/components/content/TocLinks.vue
@@ -1,7 +1,9 @@
 <template>
     <ul>
         <li v-for="link in links" :key="link.id">
-            <NuxtLink :to="{ path: route.path, hash: `#${link.id}` }"></NuxtLink>
+            <NuxtLink :to="{ path: route.path, hash: `#${link.id}` }">
+                {{ link.text }}
+            </NuxtLink>
         </li>
     </ul>
 
