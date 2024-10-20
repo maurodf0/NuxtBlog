a810a8371da99f6377c61a732ca5157c1961c20d
Mauro <maurodefalco96@gmail.com>
2024-10-20 18:32:52 +0200

    not-prose class

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -6,7 +6,7 @@
                 <div :class="{'col-span-4': doc.toc, 'col-span-6': !doc.toc}">
                     <ContentRenderer :value="doc" />
                 </div>
-                <div v-if="doc.toc" class="col-span-2">
+                <div v-if="doc.toc" class="col-span-2 not-prose">
              
                         <aside class="sticky top-8">
                             <div class="font-semibold mb-2">
