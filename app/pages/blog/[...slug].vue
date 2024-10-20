fb9b935d93c21a094f68a7d788bc038a1533e14d
Mauro <maurodefalco96@gmail.com>
2024-10-20 18:17:52 +0200

    ContentDoc with props for display TOC

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,7 +1,10 @@
 <template>
     <article 
     class="prose max-w-none dark:prose-invert prose-pre:bg-white prose-pre:text-gray-800 dark:prose-pre:text-gray-200 dark:prose-pre:bg-gray-800">
-        <ContentDoc />
+        <ContentDoc v-slot="{ doc }">
+            <pre>{{ doc.body.toc.links }}</pre>
+           <ContentRenderer :value="doc" />
+        </ContentDoc>
     </article>
 </template>
 <script setup >
