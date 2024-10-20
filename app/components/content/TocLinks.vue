8f0779cec0aecb3181e64f23acc40877a28dc471
Mauro <maurodefalco96@gmail.com>
2024-10-20 18:39:22 +0200

    toc of more levels

--- app/components/content/TocLinks.vue
+++ app/components/content/TocLinks.vue
@@ -1,9 +1,13 @@
 <template>
     <ul>
         <li v-for="link in links" :key="link.id">
-            <NuxtLink :to="{ path: route.path, hash: `#${link.id}` }">
+            <NuxtLink 
+                :to="{ path: route.path, hash: `#${link.id}` }"
+                :class="{'ml-4': level }">
                 {{ link.text }}
             </NuxtLink>
+            <TocLinks :links="link.children" :level="level +1" />
+
         </li>
     </ul>
 
