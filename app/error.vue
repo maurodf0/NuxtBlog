0b345356fb3f8ed9c467eb9f059ec5fb98d24133
Mauro <maurodefalco96@gmail.com>
2024-10-20 19:16:48 +0200

    404 page for classic Nuxt/Vue Page

---
+++ app/error.vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtLayout>
+        <section>
+            <h1 class="text-8xl mb-4">{{ error.statusCode }}</h1>
+            <h2 class="text-xl text-gray-500 dark:text-gray-400">
+                {{ error.message }}
+            </h2>
+        </section>
+    </NuxtLayout>
+</template>
+
+<script setup>
+defineProps({
+    error: Object
+})
+</script>
