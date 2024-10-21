c452a39ce44c3343708abbd50a55f72d6366d653
Mauro <maurodefalco96@gmail.com>
2024-10-22 00:58:04 +0200

    No textdecoration with prose link in tailwind

--- content/index.md
+++ content/index.md
@@ -1,6 +1,6 @@
 # Hello
 
-## I'm Mauro De Falco
+## <span class="no-underline">I'm Mauro, I'm a **web developer** and UI Designer, Marketing oriented.</span>.
 
 Read more [about me](/about), or see my latest [projects](/projects).
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,34 @@
 module.exports = {
-    darkMode: 'class',
-    plugins: [
-      require('@tailwindcss/typography'),
-      // ...
-    ],
-  }
+  darkMode: 'class',
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'h2': {
+              'text-decoration': 'none', // No text decoration
+              a: {
+                'text-decoration': 'none', // No underline on links
+              },
+            },
+            'h3': {
+              'text-decoration': 'none', // No text decoration
+              a: {
+                'text-decoration': 'none', // No underline on links
+              },
+            },
+            '@media (prefers-color-scheme: dark)': {
+              'h2': {
+                color: theme('colors.white'), // White text in dark mode
+                a: {
+                  color: theme('colors.white'), // Ensure links are also white
+                },
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
